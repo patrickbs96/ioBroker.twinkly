473707712a2171bc9371a72cd1542aac898a3a98
patrickbs96 <patrickbsimon96@gmail.com>
2020-12-09 00:59:49 +0100

    Template created

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'twinkly adapter settings': {
+        'en': 'Adapter settings for twinkly',
+        'de': 'Adaptereinstellungen für twinkly',
+        'ru': 'Настройки адаптера для twinkly',
+        'pt': 'Configurações do adaptador para twinkly',
+        'nl': 'Adapterinstellingen voor twinkly',
+        'fr': "Paramètres d'adaptateur pour twinkly",
+        'it': "Impostazioni dell'adattatore per twinkly",
+        'es': 'Ajustes del adaptador para twinkly',
+        'pl': 'Ustawienia adaptera dla twinkly',
+        'zh-cn': 'twinkly的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
